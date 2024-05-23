e3851e23415c5782c9f94c4fe69513247661f788
BrianPizz <bpizzimentijr@outlook.com>
2024-05-23 16:40:43 -0700

    filter by car make component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,25 @@
-const Filter = () => {
-    return (
-      <div>Filter</div>
-    )
-  }
-  
-  export default Filter
+import { useState } from 'react';
+
+const Filter = ({ onChange }) => {
+  const [selectedMake, setSelectedMake] = useState('');
+
+  const handleSelectChange = (event) => {
+    setSelectedMake(event.target.value);
+    onChange(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="make">Filter by Make:</label>
+      <select id="make" value={selectedMake} onChange={handleSelectChange}>
+        <option value="">All Makes</option>
+        <option value="Toyota">Toyota</option>
+        <option value="Honda">Honda</option>
+        <option value="Ford">Ford</option>
+        {/* Add more options for other car makes */}
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
